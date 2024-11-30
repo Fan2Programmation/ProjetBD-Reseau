88fe85dca977924ee6fb28ea5cbb03e37b3f0d08
Thibault <thibault.terrie.cyu@gmail.com>
2024-11-30 23:52:37 +0100

    laborieux

--- ProjetBD/ddl.sql
+++ ProjetBD/ddl.sql
@@ -1,11 +1,4 @@
 -- Créer les séquences
-CREATE SEQUENCE pers_seq
-    START 1
-    INCREMENT 1
-    MINVALUE 1
-    MAXVALUE 999999
-    CACHE 1;
-
 CREATE SEQUENCE addr_seq
     START 1
     INCREMENT 1
@@ -55,20 +48,29 @@ CREATE SEQUENCE sess_seq
     MAXVALUE 999999
     CACHE 1;
 
+-- Table Adresse
+CREATE TABLE adresse (
+    id_adresse VARCHAR(6) PRIMARY KEY DEFAULT 'Addr' || nextval('addr_seq'),
+    numero_et_voie VARCHAR(100) NOT NULL CHECK (numero_et_voie ~ '^[0-9]+[a-zA-Z\s]+$'),
+    code_postal VARCHAR(5) NOT NULL CHECK (code_postal ~ '^[0-9]{5}$'),
+    commune VARCHAR(50) NOT NULL CHECK (commune ~ '^[a-zA-Z\s]+$')
+);
+
 -- Table Identite
 CREATE TABLE identite (
     pseudo VARCHAR(27) PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(50) NOT NULL CHECK (nom ~ '^[A-Za-z]+$'),
+    prenom VARCHAR(50) NOT NULL CHECK (prenom ~ '^[A-Za-z]+$'),
     id_adresse VARCHAR(6) NOT NULL,
     telephone VARCHAR(15) UNIQUE NOT NULL CHECK (telephone ~ '^\+?[0-9]+$'),  -- Téléphone avec format spécifique
     email VARCHAR(255) UNIQUE NOT NULL CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),  -- Format email valide
-    date_naissance DATE NOT NULL
+    date_naissance DATE NOT NULL CHECK (date_naissance < CURRENT_DATE),
+    FOREIGN KEY (id_adresse) REFERENCES adresse(id_adresse)
 );
 
 -- Table Personnel
 CREATE TABLE personnel (
-    pseudo_personnel VARCHAR(27) PRIMARY KEY DEFAULT 'Pers' || nextval('pers_seq'),
+    pseudo_personnel VARCHAR(27) PRIMARY KEY,
     date_embauche_personnel DATE NOT NULL CHECK (date_embauche_personnel <= CURRENT_DATE), -- Date d'embauche <= TODAY()
     role_personnel VARCHAR(27) NOT NULL CHECK (role_personnel IN ('gestionnaire', 'technicien', 'caissier')),
     salaire FLOAT NOT NULL CHECK (salaire >= 0),
@@ -86,17 +88,8 @@ CREATE TABLE joueur (
     points_fidelite_joueur INT NOT NULL DEFAULT 0 CHECK (points_fidelite_joueur >= 0),
     solde_joueur FLOAT NOT NULL DEFAULT 0.00 CHECK (solde_joueur >= 0),
     pseudo_personnel VARCHAR(27) NOT NULL,
-    FOREIGN KEY (pseudo_personnel) REFERENCES personnel(pseudo_personnel) ON DELETE CASCADE
+    FOREIGN KEY (pseudo_personnel) REFERENCES personnel(pseudo_personnel) ON DELETE CASCADE,
     FOREIGN KEY (pseudo_joueur) REFERENCES identite(pseudo) ON DELETE CASCADE
-);
-
--- Table Adresse
-CREATE TABLE adresse (
-    id_adresse VARCHAR(6) PRIMARY KEY DEFAULT 'Addr' || nextval('addr_seq'),
-    numero_et_voie VARCHAR(100) NOT NULL CHECK (numero_et_voie ~ '^[0-9]+[a-zA-Z\s]+$'),
-    code_postal VARCHAR(5) NOT NULL CHECK (code_postal ~ '^[0-9]{5}$'),
-    commune VARCHAR(50) NOT NULL CHECK (commune ~ '^[a-zA-Z\s]+$'),
-    pseudo VARCHAR(27) NOT NULL
 );
 
 -- Table Transaction
@@ -123,9 +116,11 @@ CREATE TABLE consommable (
 -- Table Reservation
 CREATE TABLE reservation (
     id_reservation VARCHAR(7) PRIMARY KEY DEFAULT 'Res' || nextval('res_seq'),
-    date_reservation DATE NOT NULL,
-    status_reservation VARCHAR(27) NOT NULL CHECK (status_reservation IN ('en_attente', 'confirmee', 'annulee')),
+    date_debut_reservation DATE NOT NULL,
+    date_fin_reservation DATE NOT NULL,
+    status_reservation VARCHAR(27) NOT NULL CHECK (status_reservation IN ('en_attente', 'confirmee', 'annulee', 'finie')),
     pseudo_joueur VARCHAR(27) NOT NULL,
+    id_machine VARCHAR(8) NOT NULL,
     FOREIGN KEY (pseudo_joueur) REFERENCES joueur(pseudo_joueur) ON DELETE CASCADE
 );
 
@@ -135,7 +130,7 @@ CREATE TABLE machine (
     nom_machine VARCHAR(100) NOT NULL,
     emplacement_machine VARCHAR(50) NOT NULL,
     date_installation_machine DATE NOT NULL,
-    statut_machine VARCHAR(50) NOT NULL CHECK (statut_machine IN ('disponible', 'maintenance', 'horsservice')),
+    statut_machine VARCHAR(50) NOT NULL CHECK (statut_machine IN ('disponible', 'maintenance', 'hors-service')),
     nom_du_sav_machine VARCHAR(50) NOT NULL,
     numero_du_sav_machine VARCHAR(15) UNIQUE NOT NULL CHECK (numero_du_sav_machine ~ '^\+?[0-9]+$'),
     pseudo_personnel VARCHAR(27) NOT NULL,
@@ -168,15 +163,11 @@ CREATE TABLE est_classe (
 -- Table Session
 CREATE TABLE session (
     id_session VARCHAR(8) PRIMARY KEY DEFAULT 'Sess' || nextval('sess_seq'),
-    date_session DATE NOT NULL DEFAULT CURRENT_DATE,
-    score INT NOT NULL CHECK (score >= 0)
+    id_machine VARCHAR(8) NOT NULL,
+    pseudo_joueur VARCHAR(27) NOT NULL,
+    date_heure_debut DATE NOT NULL DEFAULT CURRENT_DATE,
+    date_heure_fin DATE NOT NULL DEFAULT CURRENT_DATE,
+    score INT NOT NULL CHECK (score >= 0),
+    FOREIGN KEY (id_machine) REFERENCES machine(id_machine) ON DELETE CASCADE,
+    FOREIGN KEY (pseudo_joueur) REFERENCES joueur(pseudo_joueur) ON DELETE CASCADE
 );
-
--- Ajouter les contraintes de clé étrangère après avoir créé les tables
-ALTER TABLE identite
-ADD CONSTRAINT fk_identite_adresse
-FOREIGN KEY (id_adresse) REFERENCES adresse(id_adresse);
-
-ALTER TABLE adresse
-ADD CONSTRAINT fk_adresse_identite
-FOREIGN KEY (pseudo) REFERENCES identite(pseudo);
--- ProjetBD/dml.sql
+++ ProjetBD/dml.sql
@@ -1,59 +1,60 @@
--- Insertion dans la table Adresse
-INSERT INTO Adresse (id_adresse, numero_et_voie, code_postal, commune, pseudo) VALUES
-('Addr1', '3 RUE LEBON,', '95000', 'CERGY', '2212003'),
-('Addr2', '5 BD DU PORT,', '95000', 'CERGY', '2222003');
+-- Table Identite (avec id_adresse auto-incrémenté)
+INSERT INTO adresse (numero_et_voie, code_postal, commune)
+VALUES 
+    ('3 RUE LEBON', '95000', 'CERGY'),
+    ('5 BD DU PORT', '95000', 'CERGY'),
+    ('154 AVENUE DE LA REPUBLIQUE', '95600', 'EAUBONNE');
 
--- Insertion dans la table Identite
-INSERT INTO Identite (pseudo, nom, prenom, id_adresse, telephone, email, date_naissance) VALUES
-('2212003', 'TERRIE', 'THIBAULT', 'Addr1', '+33645789125', 'terrie.thibaul@etu.cyu.fr', '2001-04-06'),
-('2222003', 'TRINH', 'GIA TAM', 'Addr2', '+33742168934', 'giatam.trinh@etu.cyu.fr', '1958-03-24');
+-- Table Identite
+INSERT INTO identite (pseudo, nom, prenom, id_adresse, telephone, email, date_naissance) 
+VALUES 
+    ('Tibsous', 'TERRIE', 'THIBAULT', 'Addr1', '+33645789125', 'thibault.terrie@etu.cyu.fr', '2001-04-06'),
+    ('Redwn', 'TRINH', 'GIATAM', 'Addr2', '+33742168934', 'giatam.trinh@etu.cyu.fr', '1958-03-24'),
+    ('el_moroccan_sniper', 'SAIDANE', 'ZAKARIA', 'Addr2', '+33615733127', 'zakaria.saidane@etu.cyu.fr', '2003-01-17'),
+    ('mlteacher', 'LEMAIRE', 'Marc', 'Addr3', '+33628990304', 'marc.lemaire@cyu.fr', '1963-08-12');
 
--- Insertion dans la table Personnel
-INSERT INTO Personnel (pseudo_personnel, date_embauche_personnel, role_personnel, salaire, status_personnel, conge_date_debut, conge_date_fin) VALUES
-('Pers11', '2000-08-08', 'gestionnaire', 2500.00, 'actif', NULL, NULL),
-('Pers21', '2000-04-06', 'caissier', 1800.00, 'congé', '2024-06-18', '2024-07-24');
+-- Table Personnel
+INSERT INTO personnel (pseudo_personnel, date_embauche_personnel, role_personnel, salaire, status_personnel, conge_date_debut, conge_date_fin) 
+VALUES
+    ('Redwn', '2024-08-08', 'gestionnaire', 2500.00, 'actif', NULL, NULL),
+    ('el_moroccan_sniper', '2024-11-27', 'caissier', 1800.00, 'congé', '2024-11-30', '2025-01-24');
 
--- Insertion dans la table Joueur
-INSERT INTO Joueur (pseudo_joueur, date_inscription_joueur, carte_fidelite_id_joueur, points_fidelite_joueur, solde_joueur, pseudo_personnel) VALUES
-('Derdoublox', '2020-01-01', 'cfhe0003', 2005, 89.79, 'Pers11'),
-('SupraFox77', '2021-01-01', 'cfst0025', 1249, 53.47, 'Pers21');
+-- Table Joueur
+INSERT INTO joueur (pseudo_joueur, date_inscription_joueur, carte_fidelite_id_joueur, points_fidelite_joueur, solde_joueur, pseudo_personnel)
+VALUES
+    ('mlteacher', '2024-08-12', 'cfst0025', 1249, 53.47, 'Redwn'),
+    ('Tibsous', '2024-11-29', 'cfhe0003', 2005, 89.79, 'el_moroccan_sniper');
 
--- Insertion dans la table Transaction
-INSERT INTO Transaction (id_transaction, date_transaction, montant_transaction, type_transaction, mode_paiement_transaction, pseudo_joueur) VALUES
-('Tran1', '2023-01-02', 10.50, 'achat_jeton', 'carte_bancaire', 'Derdoublox'),
-('Tran2', '2024-01-03', 5.00, 'achat_conso', 'carte_fidelite', 'SupraFox77');
+-- Table Transaction
+INSERT INTO transaction (date_transaction, montant_transaction, type_transaction, mode_paiement_transaction, pseudo_joueur)
+VALUES
+    ('2024-11-30', 10.50, 'achat_jeton', 'carte_bancaire', 'Tibsous'),
+    ('2024-12-03', 5.00, 'achat_conso', 'carte_fidelite', 'mlteacher');
 
--- Insertion dans la table Consommable
-INSERT INTO Consommable (id_consommable, nom_consommable, prix_consommable, stock_consommable, pseudo_joueur) VALUES
-('Cons21', 'Soda 33cl', 2.50, 100, 'Derdoublox'),
-('Cons56', 'Barre chocolatée', 1.20, 50, 'SupraFox77');
+-- Table Consommable
+INSERT INTO consommable (nom_consommable, prix_consommable, stock_consommable, pseudo_joueur)
+VALUES
+    ('Coca-Cola', 5.00, 12, 'mlteacher');
 
--- Insertion dans la table Reservation
-INSERT INTO Reservation (id_reservation, date_reservation, status_reservation, pseudo_joueur) VALUES
-('Res3457', '2024-06-18', 'en_attente', 'Derdoublox'),
-('Res8134', '2024-06-18', 'confirmee', 'SupraFox77');
+-- Table Machine
+INSERT INTO machine (nom_machine, emplacement_machine, date_installation_machine, statut_machine, nom_du_sav_machine, numero_du_sav_machine, pseudo_personnel)
+VALUES
+    ('Borne', 'Accueil', '2024-09-17', 'disponible', 'TechnoSav', '+33612345678', 'Redwn'),
+    ('Casque VR', 'Zone 3', '2024-09-19', 'maintenance', 'ServicePlus', '+33798765432', 'Redwn');
 
--- Insertion dans la table Machine
-INSERT INTO Machine (id_machine, nom_machine, emplacement_machine, date_installation_machine, statut_machine, nom_du_sav_machine, numero_du_sav_machine, pseudo_personnel, id_reservation) VALUES
-('Mach1234', 'Borne d’arcade', 'Accueil', '2000-01-01', 'disponible', 'TechnoSav', '+33612345678', 'Pers11', 'Res3457'),
-('Mach1235', 'CasqueVR', 'Zone 3', '2000-01-01', 'maintenance', 'ServicePlus', '+33798765432', 'Pers21', 'Res8134');
+-- Table Reservation
+INSERT INTO reservation (date_debut_reservation, date_fin_reservation, status_reservation, pseudo_joueur, id_machine)
+VALUES
+    ('2024-09-18 14:30:00', '2024-09-18 17:30:00', 'finie', 'mlteacher', 'Mach1'),
+    ('2024-12-18 19:00:00', '2024-06-18 21:00:00', 'confirmee', 'Tibsous', 'Mach2');
 
--- Insertion dans la table Jeu
-INSERT INTO Jeu (id_jeu, nom_jeu, categorie_jeu, date_creation_jeu, meilleur_score, id_machine) VALUES
-('Jeu15026', 'Fifa', 'jeu vidéo', '2000-01-01', 1500, 'Mach1234'),
-('Jeu56800', 'Fortnite', 'jeu vidéo', '2000-01-01', 2000, 'Mach1235');
+-- Table Jeu
+INSERT INTO jeu (nom_jeu, categorie_jeu, date_creation_jeu, id_machine)
+VALUES
+    ('Fifa', 'jeu vidéo', '1993-03-01', 'Mach1'),
+    ('Fortnite', 'jeu vidéo', '2017-09-26', 'Mach2');
 
--- Insertion dans la table est_classé
-INSERT INTO est_classé (id_jeu, pseudo_joueur, position) VALUES
-('Jeu15026', 'Derdoublox', 1),
-('Jeu56800', 'SupraFox77', 2);
-
--- Insertion dans la table Session_Jeu
-INSERT INTO Session_Jeu (id_session, date_session, score, id_jeu) VALUES
-('Sess2454', '2023-09-12', 150, 'Jeu15026'),
-('Sess2455', '2023-09-13', 200, 'Jeu56800');
-
--- Insertion dans la table joue
-INSERT INTO joue (id_session, pseudo_joueur) VALUES
-('Sess2454', 'Derdoublox'),
-('Sess2455', 'SupraFox77');
+-- Table Session
+INSERT INTO session (id_machine, pseudo_joueur, date_heure_debut, date_heure_fin, score)
+VALUES
+    ('Mach1', 'Tibsous', '2024-11-30 10:00:00', '2024-11-30 12:00:00', 512);
